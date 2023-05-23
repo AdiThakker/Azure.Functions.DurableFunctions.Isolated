1e83c9ab89f7cf9dfbfbcc1f65bedb55ad59daca
Adi Thakker <Aditya.Thakker@gmail.com>
2023-05-23 17:50:40 -0400

    Add project files.

---
+++ Azure.Functions.DurableFunctions.Isolated/DurableHttpApi.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Logging;
+using Microsoft.DurableTask;
+
+namespace Azure.Functions.DurableFunctions.Isolated
+{
+    public class DurableHttpApi
+    {
+        private ILogger Logger;
+        private string eventName = "DurableHttpApivent";
+        private string correlationId =string.Empty;
+
+        [Function(nameof(DurableHttpRequest))]
+        public async Task<HttpResponseData> DurableHttpRequest([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req, 
+                                                                [DurableClient] DurableTaskClient client, FunctionContext executionContext)
+        {
+            try
+            {
+                Logger = executionContext.GetLogger("Function1_HttpStart");
+                correlationId = Guid.NewGuid().ToString();
+                var payload = new StreamReader(req.Body).ReadToEnd();
+                var status = await client.ScheduleNewOrchestrationInstanceAsync(nameof(RunOrchestrator), payload, new StartOrchestrationOptions(correlationId));
+                Logger.LogInformation("Started orchestration with ID = '{instanceId}'.", correlationId);
+
+                var result = await client.WaitForInstanceCompletionAsync(correlationId);
+                if (result.IsCompleted)
+                {
+                    return req.CreateResponse(System.Net.HttpStatusCode.OK);
+                }
+                else
+                {
+                    await client.TerminateInstanceAsync(correlationId);
+                    return req.CreateResponse(System.Net.HttpStatusCode.InternalServerError);
+                };
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+            }
+
+            return req.CreateResponse(System.Net.HttpStatusCode.InternalServerError);
+        }
+
+        [Function(nameof(RunOrchestrator))]
+        public async Task<string> RunOrchestrator([OrchestrationTrigger] TaskOrchestrationContext context) => await context.WaitForExternalEvent<string>(eventName);
+
+        [Function(nameof(DurableHttpApiResponse))]
+        public async Task DurableHttpApiResponse([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req, [DurableClient] DurableTaskClient client)
+        {
+            await client.RaiseEventAsync(eventName, correlationId, $"I am complete CorrelationID: {correlationId}");
+        }
+    }
+}
---
+++ Azure.Functions.DurableFunctions.Isolated/HttpApi.cs
@@ -0,0 +1,48 @@
+using System.Collections.Concurrent;
+using System.Net;
+using System.Threading;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Logging;
+
+namespace Azure.Functions.DurableFunctions.Isolated
+{
+    public class HttpApi
+    {
+        private ILogger Logger;
+        private string eventName = "DurableHttpApivent";
+        private string correlationId = string.Empty;
+
+        private static ConcurrentDictionary<string, TaskCompletionSource<SampleResponse>> requests = new ConcurrentDictionary<string, TaskCompletionSource<SampleResponse>>();
+
+        [Function("HttpRequest")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, FunctionContext executionContext)
+        {
+            Logger = executionContext.GetLogger("Function1_HttpStart");
+            correlationId = Guid.NewGuid().ToString();
+            var payload = new StreamReader(req.Body).ReadToEnd();
+
+            Logger.LogInformation("Received Request for CorrleationId = '{instanceId}'.", correlationId);
+
+            TaskCompletionSource<SampleResponse> tcs = new TaskCompletionSource<SampleResponse>(correlationId);
+            requests.TryAdd(correlationId, tcs);
+            var result = await tcs.Task;
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            await response.WriteAsJsonAsync(result);
+            return response;            
+        }
+
+        [Function(nameof(HttpApiResponse))]
+        public async Task HttpApiResponse([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            var payload = new StreamReader(req.Body).ReadToEnd(); // payload is correlationId
+            if (requests.TryGetValue(payload, out TaskCompletionSource<SampleResponse> tcs))
+                tcs?.SetResult(new SampleResponse($"I am now complete for {payload}"));
+            else
+                tcs?.SetException(new Exception("Invalid request"));
+        }
+
+        public record SampleResponse(string Message);
+    }
+}
