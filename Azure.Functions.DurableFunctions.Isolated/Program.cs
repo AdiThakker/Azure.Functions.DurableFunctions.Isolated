c6a7ef196ede90b38690b0456ffdbdf24961273f
Adi Thakker <Aditya.Thakker@gmail.com>
2023-05-24 11:25:46 -0400

    DI changes and .http file

--- Azure.Functions.DurableFunctions.Isolated/Program.cs
+++ Azure.Functions.DurableFunctions.Isolated/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.DurableTask.Client;
 using Microsoft.Extensions.Hosting;
 
-var host = new HostBuilder()
+var hostBuilder = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .Build();
+    .ConfigureServices(services => services.AddDurableTaskClient(builder =>
+    {
+        // Configure options for this builder. Can be omitted if no options customization is needed.
+        builder.Configure(opt => { });
+        builder.UseGrpc(); // multiple overloads available for providing gRPC information
 
+        // AddDurableTaskClient allows for multiple named clients by passing in a name as the first argument.
+        // When using a non-default named client, you will need to make this call below to have the
+        // DurableTaskClient added directly to the DI container. Otherwise IDurableTaskClientProvider must be used
+        // to retrieve DurableTaskClients by name from the DI container. In this case, we are using the default
+        // name, so the line below is NOT required as it was already called for us.
+        builder.RegisterDirectly();
+    }));
+    
+var host =hostBuilder.Build();
 host.Run();
